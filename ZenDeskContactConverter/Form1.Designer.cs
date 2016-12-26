9ba3a9bf962f69472cffc6e09d53bc251587e13d
gigator <torrancejones@gmail.com>
2016-12-26 16:40:59 -0500

    New Feature: convert json file to csv

---
+++ ZenDeskContactConverter/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ZenDeskContactConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGO = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtInputJSON = new System.Windows.Forms.TextBox();
+            this.txtOutputCSV = new System.Windows.Forms.TextBox();
+            this.txtOutputLog = new System.Windows.Forms.TextBox();
+            this.lblInputJSON = new System.Windows.Forms.Label();
+            this.lblOutputLog = new System.Windows.Forms.Label();
+            this.lblOutputCSV = new System.Windows.Forms.Label();
+            this.pbarProgress = new System.Windows.Forms.ProgressBar();
+            this.lblErrors = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGO
+            // 
+            this.btnGO.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGO.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGO.Location = new System.Drawing.Point(638, 299);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(136, 68);
+            this.btnGO.TabIndex = 0;
+            this.btnGO.Text = "Go";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtInputJSON
+            // 
+            this.txtInputJSON.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInputJSON.Location = new System.Drawing.Point(15, 70);
+            this.txtInputJSON.Name = "txtInputJSON";
+            this.txtInputJSON.Size = new System.Drawing.Size(759, 22);
+            this.txtInputJSON.TabIndex = 1;
+            // 
+            // txtOutputCSV
+            // 
+            this.txtOutputCSV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutputCSV.Location = new System.Drawing.Point(12, 128);
+            this.txtOutputCSV.Name = "txtOutputCSV";
+            this.txtOutputCSV.Size = new System.Drawing.Size(762, 22);
+            this.txtOutputCSV.TabIndex = 2;
+            // 
+            // txtOutputLog
+            // 
+            this.txtOutputLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutputLog.Location = new System.Drawing.Point(15, 192);
+            this.txtOutputLog.Name = "txtOutputLog";
+            this.txtOutputLog.Size = new System.Drawing.Size(759, 22);
+            this.txtOutputLog.TabIndex = 3;
+            // 
+            // lblInputJSON
+            // 
+            this.lblInputJSON.AutoSize = true;
+            this.lblInputJSON.Location = new System.Drawing.Point(12, 50);
+            this.lblInputJSON.Name = "lblInputJSON";
+            this.lblInputJSON.Size = new System.Drawing.Size(80, 17);
+            this.lblInputJSON.TabIndex = 4;
+            this.lblInputJSON.Text = "Input JSON";
+            // 
+            // lblOutputLog
+            // 
+            this.lblOutputLog.AutoSize = true;
+            this.lblOutputLog.Location = new System.Drawing.Point(14, 172);
+            this.lblOutputLog.Name = "lblOutputLog";
+            this.lblOutputLog.Size = new System.Drawing.Size(79, 17);
+            this.lblOutputLog.TabIndex = 5;
+            this.lblOutputLog.Text = "Output Log";
+            // 
+            // lblOutputCSV
+            // 
+            this.lblOutputCSV.AutoSize = true;
+            this.lblOutputCSV.Location = new System.Drawing.Point(12, 108);
+            this.lblOutputCSV.Name = "lblOutputCSV";
+            this.lblOutputCSV.Size = new System.Drawing.Size(82, 17);
+            this.lblOutputCSV.TabIndex = 6;
+            this.lblOutputCSV.Text = "Output CSV";
+            // 
+            // pbarProgress
+            // 
+            this.pbarProgress.Location = new System.Drawing.Point(17, 247);
+            this.pbarProgress.Name = "pbarProgress";
+            this.pbarProgress.Size = new System.Drawing.Size(757, 23);
+            this.pbarProgress.TabIndex = 8;
+            // 
+            // lblErrors
+            // 
+            this.lblErrors.AutoSize = true;
+            this.lblErrors.Location = new System.Drawing.Point(14, 289);
+            this.lblErrors.Name = "lblErrors";
+            this.lblErrors.Size = new System.Drawing.Size(63, 17);
+            this.lblErrors.TabIndex = 9;
+            this.lblErrors.Text = "Errors: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 379);
+            this.Controls.Add(this.lblErrors);
+            this.Controls.Add(this.pbarProgress);
+            this.Controls.Add(this.lblOutputCSV);
+            this.Controls.Add(this.lblOutputLog);
+            this.Controls.Add(this.lblInputJSON);
+            this.Controls.Add(this.txtOutputLog);
+            this.Controls.Add(this.txtOutputCSV);
+            this.Controls.Add(this.txtInputJSON);
+            this.Controls.Add(this.btnGO);
+            this.Name = "Form1";
+            this.Text = "ZenDesk Contact Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtInputJSON;
+        private System.Windows.Forms.TextBox txtOutputCSV;
+        private System.Windows.Forms.TextBox txtOutputLog;
+        private System.Windows.Forms.Label lblInputJSON;
+        private System.Windows.Forms.Label lblOutputLog;
+        private System.Windows.Forms.Label lblOutputCSV;
+        private System.Windows.Forms.ProgressBar pbarProgress;
+        private System.Windows.Forms.Label lblErrors;
+    }
+}
+
---
+++ ZenDeskContactConverter/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using Newtonsoft.Json;
+
+namespace ZenDeskContactConverter
+{
+    public partial class Form1 : Form
+    {
+        private int refreshing = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // intialize file locations
+
+            txtInputJSON.Text = Application.StartupPath + "\\input\\input.json";
+            txtOutputCSV.Text = Application.StartupPath + "\\output\\output.csv";
+            txtOutputLog.Text = Application.StartupPath + "\\output\\conversion.log";
+
+        }
+
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            int errors = 0;
+
+            // prevent double clicks of the button
+            if (refreshing != 0) { return; }
+
+            refreshing++;
+            try
+            {
+                // initialize the progressbar
+                pbarProgress.Minimum = 0;
+                pbarProgress.Maximum = 100;
+                pbarProgress.Value = 0;
+
+                // ensure the file exists and other checks
+                if (!PreconditionsCheck())
+                {
+                    return;
+                }
+
+                // initialize the progressbar bounds
+                pbarProgress.Minimum = 1;
+                pbarProgress.Maximum = LinesInAFile(txtInputJSON.Text) + 1;
+
+                //
+                // read the file line by line and convert to CSV
+                //
+                using (StreamReader reader = File.OpenText(txtInputJSON.Text))
+                {
+                    
+                    string l = string.Empty;
+
+                    while ((l = reader.ReadLine()) != null)
+                    {
+                        // update the GUI
+                        pbarProgress.Value = pbarProgress.Value + 1;
+
+                        try
+                        {
+
+                            // convert the JSON string using external library
+                            dynamic json = JsonConvert.DeserializeObject(l);
+
+                            string name = json.name;
+                            string email = json.email;
+                            string external_id = json.external_id;
+                            string details = json.details;
+                            string notes = json.notes;
+                            string phone = json.phone;
+                            string rule = json.role;
+                            string restriction = json.restriction;
+
+                            dynamic jsonOrg = JsonConvert.DeserializeObject(json.organization.ToString());
+                            string organization = jsonOrg != null ? jsonOrg.name : string.Empty;
+                            string tags = "client"; // json.tags;
+
+                            WriteCSV(txtOutputCSV.Text, name, email, external_id, details, notes, phone, rule, restriction, organization, tags);
+                        }
+                        catch
+                        {
+                            errors = errors + 1;
+                        }
+                    }
+                }
+
+            }
+            finally
+            {
+                refreshing--;
+            }
+
+            // final gui refresh
+            lblErrors.Text = "Errors: " + errors.ToString();
+        }
+
+        private void WriteCSV(string path, string name, string email, string external_id, string details, string notes, 
+            string phone, string rule, string restriction, string organization, string tags)
+        {
+            name = escapeCSV(name);
+            email = escapeCSV(email);
+            external_id = escapeCSV(external_id);
+            details = escapeCSV(details);
+            notes = escapeCSV(notes);
+            phone = escapeCSV(phone);
+            rule = escapeCSV(rule);
+            restriction = escapeCSV(restriction);
+            organization = escapeCSV(organization);
+            tags = escapeCSV(tags);
+
+            string s = "\"" + name + "\",\"" + email + "\",\"" + external_id + "\",\"" + details + "\",\"" + notes + "\",\"" + phone + "\",\"" + rule + "\",\"" + restriction + "\",\"" + organization + "\",\"" + tags + "\"";
+
+            Console.WriteLine(s);
+
+            File.AppendAllText(path, s + System.Environment.NewLine);
+        }
+
+        private string escapeCSV(string field)
+        {
+            if (string.IsNullOrEmpty(field)) { return string.Empty; }
+
+            field = field.Replace("\"", "\"\"");
+            return field;
+        }
+
+        private bool PreconditionsCheck()
+        {
+            if (!FileExists(txtInputJSON.Text))
+            {
+                MessageBox.Show("Input file not found");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool FileExists(string path)
+        {
+            return string.IsNullOrEmpty(path) ? false : File.Exists(path);
+        }
+        private int LinesInAFile(string path)
+        {
+            int c = 0;
+            using (StreamReader reader = File.OpenText(path))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    c++;
+                }
+            }
+
+            return c;
+        }
+
+
+    }
+}
